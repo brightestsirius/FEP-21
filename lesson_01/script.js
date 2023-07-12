575ff653fee1c06c356ca30c157e4c5752d572e7
brightestsirius <katherina.shypovskaya@gmail.com>
2023-07-12 21:34:54 +0300

    lesson_01 – final

---
+++ lesson_01/script.js
@@ -0,0 +1,55 @@
+// console.log(100);
+// console.log(10.5);
+// console.log(10, 5, 20, 30);
+// console.log(10.2, 10.5, 30.5);
+
+// console.log("Hello, \" wor'ld!");
+// console.log("hello", "Ukraine", "Kharkiv");
+
+// console.log("hello Ukraine Kharkiv");
+
+// console.log(100, "hello", 30, "new");
+
+/**
+number => -10, 0, 10, 10.3 - blue
+srting => "", "  ", "hello" - black/red
+*/
+
+// console.log(10 + 20);
+// console.log(10 - 9.3);
+
+// console.log("Hello," + " Oleg");
+// console.log("Country" + " " + "is" + " " + "Ukraine");
+
+// console.log("Country", "is", "Ukraine");
+
+// console.log(`County is Ukraine`);
+
+// "", ``
+
+// console.log(10 + `20`); // 1020
+// console.log(`10` + 20); // 1020
+// console.log(10 + 20 + `30`); // 3030 -> NaN = not a number
+// console.log(`10` + 20 + 30); // 102030
+
+// country = `Ukraine`;
+// country = `Poland`;
+
+// console.log(country);
+
+// country = `Germany`;
+
+// console.log(country);
+
+country = `Poland`
+capital = `Warsaw`;
+
+console.log(capital + ` is a capital of ` + country);
+console.log(capital, ` is the largest city of `, country);
+
+console.log(`${capital} is a capital of ${country}. Year of birth ${2023-1990}`);
+
+x = 10;
+y = `20`;
+
+console.log(x+y);

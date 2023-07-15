29d9911d3a91474653e1dfc71a8e03bf344c38f1
brightestsirius <katherina.shypovskaya@gmail.com>
2023-07-15 09:27:00 +0300

    modified Intro.pdf && lesson_02 – intro

---
+++ lesson_02/script.js
@@ -0,0 +1,43 @@
+// combine variables
+// document.write()
+
+// increment/decrement
+
+// Math operations
+// Number/+
+// parseInt/parseFloat
+// NaN
+
+// Math.round() / Math.floor() / Math.ceil()
+// Number.toFixed()
+
+// Math.random();
+// https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Math/random
+
+// alert/prompt
+// null
+
+// string.length
+// undefined
+
+// slice()
+// toUpperCase() / toLowerCase()
+// trim()
+// replace() / replaceAll()
+
+// Boolean
+
+// Comparisons
+
+// Boolean() / !!
+
+// startsWith() / endsWith()
+// indexOf()
+// includes()
+// charAt()
+
+// if/else
+
+// isNaN()
+
+// &&, ||

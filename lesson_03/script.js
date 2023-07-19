2da7d21993fb7b6f83b8efc58abaebb795b75c19
brightestsirius <katherina.shypovskaya@gmail.com>
2023-07-19 18:44:53 +0300

    lesson_03 – intro

---
+++ lesson_03/script.js
@@ -0,0 +1,64 @@
+// string.length
+// undefined
+
+// slice()
+// toUpperCase() / toLowerCase()
+// indexOf()
+
+// Boolean
+
+// Comparisons
+
+// Boolean() / !!
+
+// startsWith() / endsWith()
+// includes()
+// charAt()
+
+// if
+// if/else
+
+// if not null
+
+// Logical operators: &&, ||
+
+// 🍰, 👩🏻‍💻
+// email: not empty, count of symbols >5, @
+
+// isNaN()
+
+// Loops
+
+// while statement
+// do...while statement
+// for statement
+
+// summ, factorial example
+
+// debugger
+
+// prime numbers
+
+// break
+// continue
+
+// loop in loop
+
+// break/continue to point
+
+// ternar
+
+// switch
+
+// Array
+
+// Array methods:
+//     push
+//     unshift
+//     pop
+//     shift
+//     join
+//     reverse
+//     slice
+//     splice
+//     concat

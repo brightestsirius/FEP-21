587b0b4269116fcaa9c58acc5c9b56b5c6eb0d65
brightestsirius <katherina.shypovskaya@gmail.com>
2023-07-22 10:17:19 +0300

    lesson_04 – intro

---
+++ lesson_04/script.js
@@ -0,0 +1,41 @@
+// Logical operators: &&, ||
+
+// 🍰, 👩🏻‍💻
+// email valid if: not empty, count of symbols >5, @ exist and not in first/last place.
+
+// isNaN()
+// yearOfBirth
+
+// Loops
+
+// while statement
+// do...while statement
+// for statement
+
+// summ, factorial example
+
+// switch
+
+// prime numbers
+
+// break
+// continue
+
+// loop in loop
+
+// break/continue to point
+
+// ternar
+
+// Array
+
+// Array methods:
+//     push
+//     unshift
+//     pop
+//     shift
+//     join
+//     reverse
+//     slice
+//     splice
+//     concat

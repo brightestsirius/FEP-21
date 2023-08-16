35a315c7c4cf50ce731b302b76d17b740ca854af
brightestsirius <katherina.shypovskaya@gmail.com>
2023-08-16 18:35:20 +0300

    lesson_11 – intro

---
+++ lesson_11/script.js
@@ -0,0 +1,37 @@
+// function as argument
+
+// callback
+
+// arrow functions
+
+// recursion
+
+// setTimeout
+// setInterval/clearInterval
+
+// Event loop
+
+// Array methods:
+//     forEach()
+//     filter()
+//     map()
+//     find() / findIndex()
+//     every / some
+//     reduce
+
+// Imperative vs Declarative Programming
+
+// Object
+// delete
+// for
+// Object.keys
+// Object.entries
+// Object.assign
+
+// function in object
+// this
+
+// Prototype
+// Object.create()
+
+// OOP

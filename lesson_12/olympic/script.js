90d1b31d6bbdac38b0d93b58829c39989fb05cb6
brightestsirius <katherina.shypovskaya@gmail.com>
2023-08-17 09:50:18 +0300

    olympic

---
+++ lesson_12/olympic/script.js
@@ -0,0 +1,92 @@
+const SPORTS = [
+	['🤺','fencing'],
+	['⛸','figure skating'],
+	['⛷','skier'],
+	['🏂','snowboarder'],
+	['🏌','golfing'],
+	['🚣','rowing boat'],
+	['🏊','swimming'],
+	['🤸','gymnastics'],
+	['🤾','handball']
+];
+
+const WINNERS = [
+	['fencing','gold','fr'],
+	['fencing','silver','it'],
+	['fencing','bronze','us'],
+
+	['figure skating','gold','ca'],
+	['figure skating','silver','pl'],
+	['figure skating','bronze','us'],
+
+	['skier','gold','no'],
+	['skier','silver','pl'],
+	['skier','bronze','fr'],
+
+	['snowboarder','gold','us'],
+	['snowboarder','silver','jp'],
+	['snowboarder','bronze','au'],
+
+	['golfing','gold','gb'],
+	['golfing','silver','se'],
+	['golfing','bronze','us'],
+
+	['rowing boat','gold','us'],
+	['rowing boat','silver','gb'],
+	['rowing boat','bronze','ro'],
+
+	['swimming','gold','us'],
+	['swimming','silver','gb'],
+	['swimming','bronze','au'],
+
+	['gymnastics','gold','pl'],
+	['gymnastics','silver','pl'],
+	['gymnastics','bronze','ua'],
+
+	['handball','gold','dk'],
+	['handball','silver','fr'],
+	['handball','bronze','de'],
+];
+
+const RINGS = ['🔵','⚫','🔴','🟡','🟢'];
+// Europe — 🔵, Africa — ⚫, America — 🔴, Asia — 🟡, Oceania — 🟢
+
+const MEDALS = [
+	['🥇','gold'],
+	['🥈','silver'],
+	['🥉','bronze'],
+];
+
+const CONTINENTS = [
+	['fr','Europe'],
+	['it','Europe'],
+	['us','America'],
+	['ca','America'],
+	['pl','Europe'],
+	['no','Europe'],
+	['jp','Asia'],
+	['au','Oceania'],
+	['gb','Europe'],
+	['se','Europe'],
+	['ro','Europe'],
+	['ua','Europe'],
+	['dk','Europe'],
+	['de','Europe']
+];
+
+const FLAGS = [
+	['fr','🇫🇷'],
+	['it','🇮🇹'],
+	['us','🇺🇸'],
+	['ca','🇨🇦'],
+	['pl','🇵🇱'],
+	['no','🇳🇴'],
+	['jp','🇯🇵'],
+	['au','🇦🇺'],
+	['gb','🇬🇧'],
+	['se','🇸🇪'],
+	['ro','🇷🇴'],
+	['ua','🇺🇦'],
+	['dk','🇩🇰'],
+	['de','🇩🇪']
+];

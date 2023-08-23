e5b9eb54cfdc0ffa28cab43e80cf33c984fcf2b5
brightestsirius <katherina.shypovskaya@gmail.com>
2023-08-23 16:58:06 +0300

    lesson_13 – intro

---
+++ lesson_13/script.js
@@ -0,0 +1,36 @@
+// Imperative vs Declarative Programming
+
+// forEach()
+// filter()
+// map()
+// find() / findIndex()
+// every() / some()
+// reduce()
+
+// setInterval/clearInterval
+
+// Object
+// delete
+// for
+// Object.keys
+// Object.entries
+// Object.assign
+
+// function in object
+// this
+// this in arrow functions
+
+// Prototype
+// Object.create()
+
+// OOP
+
+// SOLID
+
+// get/set
+
+// static methos
+
+// Object.defineProperty
+
+// call/apply/bind

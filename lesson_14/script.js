ac70132632fcf577e36ca4de9e0477780b3a0824
brightestsirius <katherina.shypovskaya@gmail.com>
2023-08-26 10:51:39 +0300

    lesson_14 – intro

---
+++ lesson_14/script.js
@@ -0,0 +1,25 @@
+// Object
+// delete
+// for
+// Object.keys
+// Object.entries
+// Object.assign
+
+// function in object
+// this
+// this in arrow functions
+
+// Prototype
+// Object.create()
+
+// OOP
+
+// SOLID
+
+// get/set
+
+// static methos
+
+// Object.defineProperty
+
+// call/apply/bind

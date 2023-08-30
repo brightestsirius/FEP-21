3076c3723c620d8947c017349b02c75069c9fed2
brightestsirius <katherina.shypovskaya@gmail.com>
2023-08-30 15:42:30 +0300

    lesson_15

---
+++ lesson_15/script.js
@@ -0,0 +1,19 @@
+// function in object
+// this
+
+// this in arrow functions
+
+// Prototype
+// Object.create()
+
+// OOP
+
+// SOLID
+
+// get/set
+
+// static methos
+
+// Object.defineProperty
+
+// call/apply/bind

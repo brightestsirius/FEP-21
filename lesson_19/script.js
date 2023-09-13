905c13ab763b76df479465768dcd6c4fedac9785
brightestsirius <katherina.shypovskaya@gmail.com>
2023-09-13 18:41:36 +0300

    lesson_19 – intro

---
+++ lesson_19/script.js
@@ -0,0 +1,46 @@
+// Everything is an object
+
+// DOM
+
+// document
+// redirect
+// querySelector
+
+// html
+// css
+// class
+
+// attributes/setAttribute
+// dataset
+// createElement
+// remove
+// events
+
+// document.createElement
+
+// click events
+//     onclick
+//     ondblclick
+//     oncontextmenu
+//     onmousedown
+//     onmouseup
+
+// addEventListener/removeEventListener
+
+// mouse events
+//     onmouseover
+//     onmouseout
+//     onmouseenter
+//     onmouseleave
+//     onmousemove
+
+// keyboard events
+//     onkeypress
+//     onkeydown
+//     onkeyup
+
+// Bubbling and capturing
+
+// stopPropagation
+
+// preventDeafault()

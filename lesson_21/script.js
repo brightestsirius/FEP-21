a6913881b82be0c7e57e08d625760dfba15cda61
brightestsirius <katherina.shypovskaya@gmail.com>
2023-09-20 18:57:58 +0300

    lesson_21

---
+++ lesson_21/script.js
@@ -0,0 +1,39 @@
+// class
+
+// attributes
+// dataset
+// remove
+// events
+
+// click events
+//     onclick
+//     ondblclick
+//     oncontextmenu
+//     onmousedown
+//     onmouseup
+
+// addEventListener/removeEventListener
+
+// document.createElement
+
+// mouse events
+//     onmouseover
+//     onmouseout
+//     onmouseenter
+//     onmouseleave
+//     onmousemove
+
+// Drag & Drop
+
+// keyboard events
+//     onkeypress
+//     onkeydown
+//     onkeyup
+
+// Bubbling and capturing
+
+// stopPropagation
+
+// preventDeafault()
+
+// call/apply/bind
